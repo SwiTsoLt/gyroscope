a2abc8ecf9c5c3d42801cb5a28e55766fe942fb6
Stanislav Smirnov <91567955+SwiTsoLt@users.noreply.github.com>
2022-11-25 00:05:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.addEventListener("deviceorientation", setDot)
 const dotElem = document.getElementById("dot")
 
 function setDot(data) {
-    const x = Math.round(data.beta)
-    const y = Math.round(data.gamma)
+    const x = Math.round(data.gamma)
+    const y = Math.round(data.beta)
     const z = Math.round(data.alpha)
     console.log(x, y);
     dotElem.style.transform = `translate(-${x}px, -${y}px)`
